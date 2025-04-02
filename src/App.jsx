0bca377bf493a42e4d3c5bc3d94a58d6bdcf3f81
lcoll813 <122918140+lcoll813@users.noreply.github.com>
2025-04-02 11:05:47 -0500

    tracking which player is nect with the history of moves, reading history and passing it to the board

--- src/App.jsx
+++ src/App.jsx
@@ -76,10 +76,18 @@ return squares[a];
 return null;
 }
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+  }
+  }
+
 return (
 <div className="game">
 <div className="game-board">  
-<Board />
+<Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
 </div>
 <div className="game-info">
   <ol>{}</ol>
